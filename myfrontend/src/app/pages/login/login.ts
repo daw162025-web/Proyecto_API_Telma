0831e381dfaf7d4ff4c68c45ff7e64bcb881cfba
daw208 <daw16.2025@gmail.com>
2026-02-02 12:49:25 +0100

    JWT

---
+++ myfrontend/src/app/pages/login/login.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app‐login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+
+export class LoginComponent {
+  email = '';
+  password = '';
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
+  
+  login() {
+    this.auth.login({ email: this.email, password: this.password }).subscribe({
+      next: () => {
+        this.router.navigate(['/profile']);
+      },
+      error: (err) => {
+        console.error('LOGIN ERROR', err);
+      },
+    });
+  }
+}
